422c88f7396106ac026c34ac5f944ca12ff65325
junaid1563 <71489463+junaid1563@users.noreply.github.com>
2025-02-10 19:11:07 +0000

    4sum

--- array/Array4Sum.java
+++ array/Array4Sum.java
@@ -1,37 +1,43 @@
 package array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Array4Sum {
     public static void main(String[] args) {
-        int[] arr = { 1, 0, -1, 0, -2, 2 };
-        System.out.println(fourSum(arr, 0));
+        // int[] arr = { 1, 0, -1, 0, -2, 2 };
+        int[] arr = { 2, 2, 2, 2, 2, 2 };
+        System.out.println(fourSum(arr, 8));
     }
 
     public static List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
 
-                int sum = nums[i] + nums[j];
+                long sum = nums[i] + nums[j];
                 int lo = j + 1, hi = nums.length - 1;
                 List<List<Integer>> twoSum = twoSum(nums, lo, hi, target - sum);
                 System.out.println("Two sum = " + twoSum.toString());
                 for (List<Integer> list : twoSum) {
                     list.add(nums[i]);
                     list.add(nums[j]);
+                    if (!ans.contains(list)) {
+                        ans.add(list);
+                    }
                 }
-                ans.addAll(twoSum);
+
             }
         }
         return ans;
     }
 
-    public static List<List<Integer>> twoSum(int[] nums, int lo, int hi, int target) {
+    public static List<List<Integer>> twoSum(int[] nums, int lo, int hi, long target) {
         List<List<Integer>> ans = new ArrayList<>();
         while (lo < hi) {
-            int sum = nums[lo] + nums[hi];
+            long sum = nums[lo] + nums[hi];
             if (sum == target) {
                 List<Integer> list = new ArrayList<>();
                 list.add(nums[lo]);
