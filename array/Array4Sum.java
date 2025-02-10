66447dbb52b33720a537490ca84fb0b37ee5c3c5
junaid1563 <junaidajmer87@gmail.com>
2025-02-11 01:03:56 +0530

    updating notes

--- array/Array4Sum.java
+++ array/Array4Sum.java
@@ -5,18 +5,27 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Array4Sum {
+
+    // sort array, fix 2 points and then find other 2 points with two sum method for
+    // sorted array
+    // https://leetcode.com/problems/4sum/
     public static void main(String[] args) {
-        // int[] arr = { 1, 0, -1, 0, -2, 2 };
-        int[] arr = { 2, 2, 2, 2, 2, 2 };
-        System.out.println(fourSum(arr, 8));
+        int[] arr = { 1, 0, -1, 0, -2, 2 };
+        // int[] arr = { 2, 2, 2, 2, 2, 2 };
+        System.out.println(fourSum(arr, 0));
     }
 
     public static List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j - 1] == nums[j]) {
+                    continue;
+                }
                 long sum = nums[i] + nums[j];
                 int lo = j + 1, hi = nums.length - 1;
                 List<List<Integer>> twoSum = twoSum(nums, lo, hi, target - sum);
