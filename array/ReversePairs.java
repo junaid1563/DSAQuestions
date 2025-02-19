783094141dbfa8a219b92991072790ccf620a29a
junaid1563 <junaidajmer87@gmail.com>
2025-02-20 01:23:20 +0530

    not  done

---
+++ array/ReversePairs.java
@@ -0,0 +1,20 @@
+public class ReversePairs{
+    // TLE 
+    // not optimized
+    public static void main(String[] args) {
+        
+    }
+    public int reversePairs(int[] nums) {
+        int op=0;
+        for(int i=0;i<nums.length-1;i++){
+            long iv=nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                long jv=nums[j];
+                if(iv>2*jv){
+                    op++;
+                }
+            }
+        }
+    return op;
+    }
+}
