5d009a4211a0b422f374f4e62c97109214a1f82e
junaid1563 <71489463+junaid1563@users.noreply.github.com>
2025-02-28 19:58:43 +0000

    rotate matrix

--- strings/RotateImage.java
+++ strings/RotateImage.java
@@ -1,9 +1,15 @@
 package strings;
 
+/*
+ * approach
+ * 1. Find transpose 
+ * 2. Find reverse of transpose 
+ * https://leetcode.com/problems/rotate-image/description/
+*/
 public class RotateImage {
     public static void main(String[] args) {
         int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        transpose(matrix);
+        rotate(matrix);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
@@ -12,9 +18,9 @@ public class RotateImage {
         }
     }
 
-    public static void transpose(int [][]matrix){
-        for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix[i].length;j++){
+    public static void transpose(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[i].length; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
@@ -22,23 +28,21 @@ public class RotateImage {
         }
     }
 
-    public static void rotate(int[][] matrix) {
-        // int[][] ans = new int[matrix.length][matrix.length];
+    public static void reverse(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = i; j < matrix[i].length; j++) {
-                int idx = matrix.length - i - 1;
-                // ans[j][idx] = matrix[i][j];
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][idx];
-                matrix[j][idx] = temp;
+            int row = i, left = 0, right = matrix[i].length - 1;
+            while (left < right) {
+                int temp = matrix[row][left];
+                matrix[row][left] = matrix[row][right];
+                matrix[row][right] = temp;
+                left++;
+                right--;
             }
         }
-        // matrix = ans;
-        // for (int i = 0; i < matrix.length; i++) {
-        //     for (int j = 0; j < matrix[i].length; j++) {
-        //         System.out.print(matrix[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
+    }
+
+    public static void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverse(matrix);
     }
 }
