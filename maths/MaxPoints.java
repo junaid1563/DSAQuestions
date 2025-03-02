9a045ea710c0fb44ae0500f6fb06c2bd76f9d78b
junaid1563 <junaidajmer87@gmail.com>
2025-03-02 23:44:54 +0530

    partial

--- maths/MaxPoints.java
+++ maths/MaxPoints.java
@@ -7,7 +7,7 @@ import java.util.Map.Entry;
 // Not completed
 public class MaxPoints {
     public int maxPoints(int[][] points) {
-        int count = Integer.MIN_VALUE;
+        int count =1;
         int n = points.length;
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
@@ -15,7 +15,7 @@ public class MaxPoints {
                 int x1 = points[i][0], y1 = points[i][1];
                 int x2 = points[j][0], y2 = points[j][1];
                 int slope = 0;
-                if (x1 == x2) {
+                if (x1 == x2 || y2==y1) {
                     continue;
                 }else{
 
@@ -29,8 +29,9 @@ public class MaxPoints {
                 }
             }
         }
+        System.out.println(map.toString());
         for (Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue()!=-1 && entry.getValue() > count) {
+            if (entry.getKey()!=-1  && entry.getValue() > count) {
                 count = entry.getValue();
             }
         }
